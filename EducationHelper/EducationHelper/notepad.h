331298e15425d3536c2b81c6b84d9ad32ba85ea2
HimanthaRN <himanthananayakkara@gmail.com>
2021-01-03 15:08:25 +0530

    notepade function commenting

--- EducationHelper/EducationHelper/notepad.h
+++ EducationHelper/EducationHelper/notepad.h
@@ -17,28 +17,30 @@ public:
     ~NotePad();
 
 private slots:
+    //declaring all slots for a notepad
+    //new page function
     void on_actionNewPage_triggered();
-
+    //open
     void on_actionOpen_triggered();
-
+    //save the page
     void on_actionSave_triggered();
-
+    //save as function
     void on_actionSaveAs_triggered();
-
+    //cut function
     void on_actionCut_triggered();
-
+//copy function
     void on_actionCopy_triggered();
-
+//paste function
     void on_actionPaste_triggered();
-
+// redo function
     void on_actionRedo_triggered();
-
+//undo function
     void on_actionUndo_triggered();
-
+//Calculator trigger for moving to calculator page when it clicked
     void on_actionCalculator_triggered();
-
+//video player trigger for moving to calculator page when it clicked
     void on_actionVideo_triggered();
-
+//about and aims of the application
     void on_actionAbout_triggered();
 
 private:
